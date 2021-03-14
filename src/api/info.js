6bc209ad2cd0a971faed24352c28a655b5e29852
Dhanish Kumar <dhanish@mobiux.in>
2021-03-14 14:55:51 +0530

    Added more fields in personal details object

--- src/api/info.js
+++ src/api/info.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const personalDetails = {
+  name: 'Dhanish Kumar S',
+  dateOfBirth: '27-09-1997',
+  college: 'College Name',
+  branch: 'Branch Name',
+  yearOfJoining: 2015,
+  graduationYear: 2019,
+  gitHubProfile: 'https://github.com/mygithubprofile',
+  linkdinProfile: 'https://www.linkedin.com/in/myprofile/',
+  languages: ['Languaage1', 'Language2', 'Language3'],
+  programmingLanguages: ['javascript', 'python']
+};
+
 router.get('/', (req, res) => {
-  res.json({
-    name: 'Dhanish Kumar S'
-  });
+  res.json(personalDetails);
 });
 
 module.exports = router;
